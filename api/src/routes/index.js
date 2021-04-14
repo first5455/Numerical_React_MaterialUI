451559f3d290c3a4dbb17973028ea82741efc40a
first5455 <first545455@gmail.com>
2021-04-15 01:58:30 +0700

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,6 +5,13 @@ mongoose
   .connect("mongodb://mongos:27017/mydatabase")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
+
+//for Develop only
+/* mongoose
+  .connect("mongodb://localhost:27017/mydatabase")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err)); */
+
 const Controller = require("../controllers/controllers");
 const { schema } = require("../models/example");
 
@@ -18,6 +25,36 @@ const routes = [
     method: "GET", //
     url: "/api/example/:name",
     handler: Controller.getSingleExample,
+    schema:{
+      params:{
+        type: 'object',
+        properties:{
+          name:{
+            type: 'string',
+            description: 'Name of Method'
+          },
+        }
+      },
+      response:{
+        200:{
+          description: 'Successful Response',
+          type: 'object',
+          properties:{
+            name: {type: 'string'},
+            xl: {type: 'number'},
+            xr: {type: 'number'},
+            latex: {type: 'string'},
+            x: {type: 'number'},
+            arrayA: {type: 'array'},
+            arrayB: {type: 'array'},
+            arrayX: {type: 'array'},
+            arrayY: {type: 'array'},
+            xfind: {type: 'number'},
+            arrayInput: {type: 'string'},
+          }
+        }
+      }
+    },
   },
   /*   {
     method: "POST",

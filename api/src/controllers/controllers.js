5ca97054af1622e3cdc86a836715c224b0a1a1c7
first5455 <first545455@gmail.com>
2021-04-21 20:16:01 +0700

    updated

--- api/src/controllers/controllers.js
+++ api/src/controllers/controllers.js
@@ -24,37 +24,4 @@ exports.getSingleExample = async (req, reply) => {
     throw boom.boomify(err)
   }
 }
-/* 
-// Add a new
-exports.addExample = async (req, reply) => {
-  try {
-    const example = new Example(req.body)
-    return example.save()
-  } catch (err) {
-    throw boom.boomify(err)
-  }
-}
 
-// Update an existing
-exports.updateExample = async (req, reply) => {
-  try {
-    const name = req.params.name
-    const example = req.body
-    const { ...updateData } = example
-    const update = await Example.findOneAndUpdate({name:name}, updateData, { new: true })
-    return update
-  } catch (err) {
-    throw boom.boomify(err)
-  }
-}
-
-// Delete
-exports.deleteExample = async (req, reply) => {
-  try {
-    const name = req.params.name
-    const example = await Example.findOneAndRemove({name:name})
-    return example
-  } catch (err) {
-    throw boom.boomify(err)
-  }
-} */
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -10,7 +10,6 @@ mongoose
   .catch((err) => console.log(err));
 
 const Controller = require("../controllers/controllers");
-const { schema } = require("../models/example");
 
 const routes = [
   {
@@ -47,120 +46,11 @@ const routes = [
             arrayX: { type: "array" },
             arrayY: { type: "array" },
             xfind: { type: "number" },
-            arrayInput: { type: "string" },
           },
         },
       },
     },
   },
-/*   {
-    method: "POST",
-    url: "/api/example",
-    handler: Controller.addExample,
-    schema: {
-      body: {
-        type: "object",
-        properties: {
-          name: {type: 'string'}
-        }
-      },
-      response: {
-        200: {
-          description: "Successful Response",
-          type: "object",
-          properties: {
-            name: { type: "string" },
-            xl: { type: "number" },
-            xr: { type: "number" },
-            latex: { type: "string" },
-            x: { type: "number" },
-            arrayA: { type: "array" },
-            arrayB: { type: "array" },
-            arrayX: { type: "array" },
-            arrayY: { type: "array" },
-            xfind: { type: "number" },
-            arrayInput: { type: "string" },
-          },
-        },
-      },
-    },
-  },
-  {
-    method: "PUT",
-    url: "/api/example/:name",
-    handler: Controller.updateExample,
-    schema: {
-      body: {
-        type: "object",
-        properties: {
-          name: { type: 'string'}
-        }
-      },
-      params: {
-        type: "object",
-        properties: {
-          name: {
-            type: "string",
-            description: "Name of Method",
-          },
-        },
-      },
-      response: {
-        200: {
-          description: "Successful Response",
-          type: "object",
-          properties: {
-            name: { type: "string" },
-            xl: { type: "number" },
-            xr: { type: "number" },
-            latex: { type: "string" },
-            x: { type: "number" },
-            arrayA: { type: "array" },
-            arrayB: { type: "array" },
-            arrayX: { type: "array" },
-            arrayY: { type: "array" },
-            xfind: { type: "number" },
-            arrayInput: { type: "string" },
-          },
-        },
-      },
-    },
-  },
-  {
-    method: "DELETE",
-    url: "/api/example/:name",
-    handler: Controller.deleteExample,
-    schema: {
-      params: {
-        type: "object",
-        properties: {
-          name: {
-            type: "string",
-            description: "Name of Method",
-          },
-        },
-      },
-      response: {
-        200: {
-          description: "Successful Response",
-          type: "object",
-          properties: {
-            name: { type: "string" },
-            xl: { type: "number" },
-            xr: { type: "number" },
-            latex: { type: "string" },
-            x: { type: "number" },
-            arrayA: { type: "array" },
-            arrayB: { type: "array" },
-            arrayX: { type: "array" },
-            arrayY: { type: "array" },
-            xfind: { type: "number" },
-            arrayInput: { type: "string" },
-          },
-        },
-      },
-    },
-  }, */
 ];
 
 module.exports = routes;
